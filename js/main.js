d4a3ddadf410e4c956614839babde675759444db
IsSardor <140827704+IsSardor@users.noreply.github.com>
2023-07-28 19:11:01 +0300

    Tuit
    Initial project load

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper-container', {
+  spaceBetween: 30,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+});
